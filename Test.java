974074977027be13fe21134033e71a9f2174b8ae
TonyMontoyaO <amontoya.olivas15@gmail.com>
2024-09-04 11:58:35 -0600

    Method Example

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class A
+{
+    int n , z ;
+    static int m =100;//static variable
+    void method()
+    {
+        n =90;//local variable
+    }
+    public static void main(String args[])
+    {
+        z =50;//instance variable
+    }
+}//end of class   
