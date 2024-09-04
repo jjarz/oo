414c208c664b4d326172f6aca44c6d398743bdb5
KatlynnGaster <kittygaster@gmail.com>
2024-09-04 11:57:03 -0600

    Class example with abstraction

---
+++ MathProblem.java
@@ -0,0 +1,17 @@
+public abstract class MathProblem {
+    protected int operandA = 0;
+    protected int operandB = 0;
+
+    public MathProblem(int opA, int opB) {
+        operandA = opA;
+        operandB = opB;
+    }
+
+    public abstract int getAnswer();
+
+    public abstract char getOperatorCharacter();
+
+    public String toString() {
+        return "[" + operandA + getOperatorCharacter() + operandB + "=" + getAnswer() + "]";
+    }
+}
